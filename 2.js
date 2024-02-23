6d5139ae6ba3bad139f8bc06d29646089aa0e92c
ingmapache <mapache.rgp@gmail.com>
2024-02-23 00:12:28 -0600

    Add excercise

---
+++ 2.js
@@ -0,0 +1,9 @@
+var map = function(arr, fn) {
+    let returnedArray = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        returnedArray[i] = fn(arr[i], i);
+    }
+    
+    return returnedArray;
+};
