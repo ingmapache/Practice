8fb27373c31fca8b36bdb716a0754f1c29f87619
ingmapache <mapache.rgp@gmail.com>
2024-02-29 16:34:21 -0600

    UPGRADE JS / HTML / CSS
    Added input boxes so user can type min and max numbers, and made
    some rules in the function so user gets alerted if expected numbers
    for the function are wrong.

--- Random_num_gen/rng.js
+++ Random_num_gen/rng.js
@@ -1,11 +1,18 @@
 const randomButton = document.getElementsByClassName("randomNumButton")[0];
-let min = 10;
-let max = 20;
 let numberSpot = document.getElementsByClassName("randomNumber")[0];
 
-
-
 randomButton.onclick = function() {
-    numberSpot.textContent = Math.floor(Math.random() * max) + min;
+    const min = document.getElementById("minNumber").valueAsNumber;
+    const max = document.getElementById("maxNumber").valueAsNumber;
+    if (min <= 0) {
+        alert("Minimum number must be positive and greater than 0");
+        return;
+    }
+    if (max <= 0 || max <= min) {
+        alert("Maximum number must be positive and greater than minimum number");
+        return;
+    }
+    numberSpot.textContent = Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(document.getElementById("minNumber").valueAsNumber);    
 }
 
