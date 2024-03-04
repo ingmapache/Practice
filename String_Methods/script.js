2745924f1b8cb5175097614e9c08f8276301bbda
ingmapache <mapache.rgp@gmail.com>
2024-03-03 21:12:30 -0600

    ADD string methods

---
+++ String_Methods/script.js
@@ -0,0 +1,11 @@
+const length = "I like pineapples";
+console.log(`The length of "${length}" is "${length.length}".`)
+
+const charat = "Jujutsu Kaisen";
+console.log(`The 5th character in "${charat}" is "${charat.charAt(4)}".`)
+
+const charcodeat = "Iphones are good";
+console.log(`The unicode of the 6th character in "${charcodeat}" is "${charcodeat.charCodeAt(5)}".`);
+
+const slice = "Banana or Apple";
+console.log(`The string: "${slice}" sliced from 0 to 6 is "${slice.slice(0, 6)}"`);
